2425836ad0ac00ca62a143ef2d2edb697882f1ba
Théo <theo.cartegnie@le-campus-numerique.fr>
2025-07-22 13:53:32 +0200

    First commit with files

---
+++ resources/views/app.blade.php
@@ -0,0 +1,4 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @class(['dark' => ($appearance ?? 'system') == 'dark'])>
+  <h1>Hello World From Laravel v12.x (PHPv8.x.x)</h1>
+</html>
